b8a92dffe530de1a4d91f0c2b1458683a875a4f1
simovanev <simovanev@abv.bg>
2022-11-09 04:06:04 +0200

    Exercise Associative Arrays

--- src/associativeArrays/legendaryFarming.java
+++ src/associativeArrays/legendaryFarming.java
@@ -13,42 +13,56 @@ public class legendaryFarming {
         farmMap.put("fragments", 0);
         farmMap.put("motes", 0);
         LinkedHashMap<String, Integer> junk = new LinkedHashMap<>();
-        String[] input = scanner.nextLine().split("\\s+");
+        String line= scanner.nextLine();
         boolean win = false;
-        for (int i = 0; i < input.length - 1; i++) {
-            if (i % 2 == 0) {
-                int points = 0;
-                String material = "";
-                points = Integer.parseInt(input[i]);
-                material = input[i + 1].toLowerCase();
-                if (material.equals("shards") || material.equals("fragments") || material.equals("motes")) {
+        while (!win) {
+            String[] input = line.split("\\s+");
+            for (int i = 0; i < input.length - 1; i++) {
+                if (i % 2 == 0) {
+                    int points = 0;
+                    String material = "";
+                    points = Integer.parseInt(input[i]);
+                    material = input[i + 1].toLowerCase();
+                    if (material.equals("shards") || material.equals("fragments") || material.equals("motes")) {
 
-                    if (!win) {
-                        farmMap.put(material, farmMap.get(material) + points);
+                      if (!win) {
+                            farmMap.put(material, farmMap.get(material) + points);
+                       }
+
+                        if (farmMap.get("shards") >= 250) {
+                            System.out.println("Shadowmourne obtained!");
+                            farmMap.put("shards", farmMap.get("shards") - 250);
+                            win = true;
+                            break;
+
+
+                        } else if (farmMap.get("fragments") >= 250) {
+                            System.out.println("Valanyr obtained!");
+                            farmMap.put("fragments", farmMap.get("fragments") - 250);
+                            win = true;
+                            break;
+
+
+                        } else if (farmMap.get("motes") >= 250) {
+                            System.out.println("Dragonwrath obtained!");
+                            farmMap.put("motes", farmMap.get("motes") - 250);
+                            win = true;
+                            break;
+
+                        }
+
+
+                    } else {
+                        junk.putIfAbsent(material, 0);
+                        junk.put(material, junk.get(material) + points);
                     }
-
-                    if (farmMap.get("shards") >= 250) {
-                        System.out.println("Shadowmourne obtained!");
-                        farmMap.put("shards", farmMap.get("shards") - 250);
-                        win = true;
-
-                    } else if (farmMap.get("fragments") >= 250) {
-                        System.out.println("Valanyr obtained!");
-                        farmMap.put("fragments", farmMap.get("fragments") - 250);
-                        win = true;
-
-                    } else if (farmMap.get("motes") >= 250) {
-                        System.out.println("Dragonwrath obtained!");
-                        farmMap.put("motes", farmMap.get("motes") - 250);
-                        win = true;
-                    }
-
-
-                } else {
-                    junk.putIfAbsent(material, 0);
-                    junk.put(material, junk.get(material) + points);
                 }
             }
+            if (win) {
+
+                break;
+            }
+            line= scanner.nextLine();
         }
         for (Map.Entry<String, Integer> element : farmMap.entrySet()) {
             System.out.println(element.getKey() + ": " + element.getValue());
