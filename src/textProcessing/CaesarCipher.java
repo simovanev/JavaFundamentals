3eb42ffc62dc0687e9b7e983cc511bb9d8dda415
simovanev <simovanev@abv.bg>
2022-11-14 04:11:21 +0200

    Exercise Text Processing

--- src/textProcessing/CaesarCipher.java
+++ src/textProcessing/CaesarCipher.java
@@ -1,4 +1,14 @@
 package textProcessing;
 
+import java.util.Scanner;
+
 public class CaesarCipher {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String text= scanner.nextLine();
+        for (char symbol: text.toCharArray()){
+            char modifiedSymbol=(char)(symbol+3);
+            System.out.print(modifiedSymbol);
+        }
+    }
 }
