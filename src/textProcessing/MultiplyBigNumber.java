156d3af97199e3b18726fe56db57265f0175d95a
simovanev <simovanev@abv.bg>
2022-11-15 04:52:52 +0200

    Exercise Text Processing

--- src/textProcessing/MultiplyBigNumber.java
+++ src/textProcessing/MultiplyBigNumber.java
@@ -6,23 +6,26 @@ public class MultiplyBigNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String number = scanner.nextLine();
+
         int digit = Integer.parseInt(scanner.nextLine());
         StringBuilder multiNum = new StringBuilder();
-        int remain=0;
+        int remain = 0;
         for (int i = number.length() - 1; i >= 0; i--) {
-            int currentNum = Integer.parseInt(String.valueOf(number.charAt(i))) * digit+remain;
+            int currentNum = Integer.parseInt(String.valueOf(number.charAt(i))) * digit + remain;
             remain = currentNum / 10;
             multiNum.append(currentNum % 10);
         }
-       if (remain>0){
-           multiNum.append(remain);
-       }
-       if (digit==0 ){
-           System.out.println(0);
-       }else {
-        System.out.println(multiNum.reverse());
+        if (remain > 0) {
+            multiNum.append(remain);
+        }
+        multiNum.reverse();
+        if (digit == 0) {
+            System.out.println(0);
+        } else if (multiNum.charAt(0)=='0') {
+            multiNum.deleteCharAt(0);
+            System.out.println(multiNum);
 
-       }
+        }else System.out.println(multiNum);
 
     }
 }
--- src/textProcessing/StringExplosion.java
+++ src/textProcessing/StringExplosion.java
@@ -1,4 +1,23 @@
 package textProcessing;
 
+import java.util.Scanner;
+
 public class StringExplosion {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        StringBuilder text=new StringBuilder(scanner.nextLine());
+        int force=0;
+        for (int i = 0; i < text.length(); i++) {
+            char symbol= text.charAt(i);
+            if (symbol=='>'){
+                force+=Integer.parseInt(String.valueOf(text.charAt(i+1)));
+            } else if (symbol!='>' && force>0) {
+                text.deleteCharAt(i);
+                force--;
+                i--;
+
+            }
+        }
+        System.out.println(text);
+    }
 }
