7f2cf8b35f0d0562a807e5cd845261bd6acc8e87
simovanev <simovanev@abv.bg>
2022-11-24 04:32:02 +0200

    Exercise Text Processing

--- src/finalExamPreparation/ActivationKey.java
+++ src/finalExamPreparation/ActivationKey.java
@@ -1,5 +1,6 @@
 package finalExamPreparation;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ActivationKey {
@@ -8,41 +9,49 @@ public class ActivationKey {
         StringBuilder key = new StringBuilder(scanner.nextLine());
         String command = scanner.nextLine();
         while (!command.equals("Generate")) {
-            String action = command.split(">>>")[0];
+            String[] sequence = command.split(">>>");
+            String action = sequence[0];
             if (action.equals("Contains")) {
-                String substring = command.split(">>>")[1];
+                String substring = sequence[1];
                 if (key.toString().contains(substring)) {
                     System.out.printf("%s contains %s\n", key, substring);
                 } else System.out.println("Substring not found!");
 
             } else if (action.equals("Flip")) {
-                String substring = command.split(">>>")[1];
-                int startIndex = Integer.parseInt(command.split(">>>")[2]);
+                String lowerOrUpper = sequence[1];
+                int startIndex = Integer.parseInt(sequence[2]);
                 int endIndex = key.length() - 1;
-                if (command.length() == 3) {
-                    endIndex = Integer.parseInt(command.split(">>>")[3]);
+                if (sequence.length == 4) {
+                    endIndex = Integer.parseInt(sequence[3]);
                 }
-                if (substring.equals("Upper")) {
-                    key.substring(startIndex,endIndex).toUpperCase();
-                } else if (substring.equals("lower")) {
-                    key.substring(startIndex,endIndex).toLowerCase();
+                String subText = key.substring(startIndex, endIndex);
+                if (lowerOrUpper.equals("Upper")) {
+                    String newText = subText.toUpperCase();
+                    key.replace(startIndex, endIndex, newText);
+
+
+                } else if (lowerOrUpper.equals("Lower")) {
+                    String newText = subText.toLowerCase();
+                    key.replace(startIndex, endIndex, newText);
+
                 }
                 System.out.println(key);
 
 
             } else if (action.equals("Slice")) {
-                int startIndex = Integer.parseInt(command.split(">>>")[1]);
+                int startIndex = Integer.parseInt(sequence[1]);
                 int endIndex = key.length() - 1;
-                if (command.length() == 2) {
-                    endIndex = Integer.parseInt(command.split(">>>")[2]);
+                if (sequence.length == 3) {
+                    endIndex = Integer.parseInt(sequence[2]);
                 }
-                String substring=key.substring(startIndex,endIndex);
-                key.delete(startIndex,endIndex);
+
+                key.delete(startIndex, endIndex);
                 System.out.println(key);
             }
 
 
             command = scanner.nextLine();
         }
+        System.out.printf("Your activation key is: %s",key);
     }
 }
