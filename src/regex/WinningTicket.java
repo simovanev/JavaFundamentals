8c3c5ab1a6a5f613e3ef558bcd3d34f50bb9e68d
simovanev <simovanev@abv.bg>
2022-12-23 09:40:57 +0200

    Regular expressions Exercise

---
+++ src/regex/WinningTicket.java
@@ -0,0 +1,10 @@
+package regex;
+
+import java.util.Scanner;
+
+public class WinningTicket {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+    }
+}
