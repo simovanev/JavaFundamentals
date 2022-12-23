1ad259ea574a711031b06b9580072d08969326ae
simovanev <simovanev@abv.bg>
2022-12-23 12:17:26 +0200

    Regular expressions Exercise

--- src/regex/WinningTicket.java
+++ src/regex/WinningTicket.java
@@ -1,10 +1,17 @@
 package regex;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class WinningTicket {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
+        String[] input= scanner.nextLine().split(", ");
+        String regex="";
+        Pattern pattern= Pattern.compile(regex);
+        for (int i = 0; i < input.length; i++) {
+
+        }
 
     }
 }
