752d39de35fc706eda4992f65d4dff1bdb772187
simovanev <simovanev@abv.bg>
2022-12-25 10:34:55 +0200

    Regular expressions Exercise

--- src/regex/WinningTicket.java
+++ src/regex/WinningTicket.java
@@ -7,9 +7,13 @@ public class WinningTicket {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         String[] input= scanner.nextLine().split(", ");
-        String regex="";
+        String regex6="([@#^$])\\1{5}";
         Pattern pattern= Pattern.compile(regex);
         for (int i = 0; i < input.length; i++) {
+
+            String first=input[i].substring(0,input[i].length()/2);
+            String second=input[i].substring(0,input[i].length()/2);
+            if ()
 
         }
 
