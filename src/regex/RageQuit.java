572f3a7a8c20adcaa95053b1248cc8b521b48ccf
simovanev <simovanev@abv.bg>
2022-12-31 11:35:17 +0200

    Regular expressions Exercise

--- src/regex/RageQuit.java
+++ src/regex/RageQuit.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 
 public class RageQuit {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in)
+        Scanner scanner=new Scanner(System.in);
     }
 }
